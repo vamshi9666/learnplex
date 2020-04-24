16e593b149c978167533c988ab0ed51fc360fe7e
Bhanu Teja P <pbteja1998@gmail.com>
2020-04-24 19:21:02 +0530

    Setup ant-design based on example from mit123suki/next.js repo
    https://github.com/zeit/next.js/pull/11837

---
+++ src/pages/_app.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { AppProps } from 'next/app'
+import 'antd/dist/antd.css'
+
+import '../styles/vars.css'
+import '../styles/global.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+    return <Component {...pageProps} />
+}
+
+export default MyApp
