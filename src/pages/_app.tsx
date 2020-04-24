9610ebcd72f54bafb5783608d4f12bc7f07408c8
Bhanu Teja P <pbteja1998@gmail.com>
2020-04-24 22:20:23 +0530

    Setup next-urql client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { AppProps } from 'next/app'
+import { withUrqlClient } from 'next-urql'
 import 'antd/dist/antd.css'
 
 import '../styles/vars.css'
@@ -10,4 +11,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return <App Component={<Component {...pageProps} />} />
 }
 
-export default MyApp
+export default withUrqlClient({
+  url:
+    process.env.NODE_ENV === 'production'
+      ? (process.env.GQL_ENDPOINT as string)
+      : 'http://localhost:4000/graphql',
+  requestPolicy: 'network-only',
+})(MyApp)
