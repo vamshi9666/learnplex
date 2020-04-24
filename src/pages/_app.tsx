686a7947cc0c179d56c51eef1e06c1916523f1e4
Bhanu Teja P <pbteja1998@gmail.com>
2020-04-24 19:48:47 +0530

    Lint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 import { AppProps } from 'next/app'
 import 'antd/dist/antd.css'
 
@@ -6,7 +6,7 @@ import '../styles/vars.css'
 import '../styles/global.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
 
 export default MyApp
