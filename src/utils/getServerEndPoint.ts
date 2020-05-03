83766da1baff005c6fef75c4634f4e9dc677b970
Bhanu Teja P <pbteja1998@gmail.com>
2020-05-04 00:25:06 +0530

    Fix internal server error when running production build with next build && next start
    https://github.com/mit123suki/next-ant-css/issues/2

--- src/utils/getServerEndPoint.ts
+++ src/utils/getServerEndPoint.ts
@@ -1,5 +1,5 @@
 export function getServerEndPoint() {
   return process.env.NODE_ENV === 'production'
-    ? (process.env.SERVER_ENDPOINT as string)
+    ? (process.env.SERVER_ENDPOINT as string) ?? 'http://localhost:4000'
     : 'http://localhost:4000'
 }
