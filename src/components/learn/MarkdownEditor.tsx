2c7b94358f9a7964a4c034bdd54dd46c233a39bb
Bhanu Teja P <pbteja1998@gmail.com>
2020-05-09 03:42:20 +0530

    Add ability to add youtube videos using markdown
    - https://github.com/markdown-it/markdown-it-container

--- src/components/learn/MarkdownEditor.tsx
+++ src/components/learn/MarkdownEditor.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import dynamic from 'next/dynamic'
 import MarkdownIt from 'markdown-it'
 import insert from 'markdown-it-ins'
+import markdownItContainer from 'markdown-it-container'
 import hljs from 'highlight.js'
 import { Skeleton } from 'antd'
+import Token from 'markdown-it/lib/token'
 
 import { KeyboardShortcuts } from './lib/KeyboardShortcuts'
 
@@ -13,6 +15,22 @@ const MdEditor = dynamic(() => import('react-markdown-editor-lite'), {
 let KeyboardEventHandler: any
 if (process.browser) {
   KeyboardEventHandler = require('react-keyboard-event-handler')
+}
+
+function getYoutubeVideoId(url: string) {
+  const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/
+  const match = url.match(regExp)
+
+  return match && match[2].length === 11 ? match[2] : null
+}
+
+const getYoutubeIframeMarkup = ({ url }: { url: string }) => {
+  console.log({ url })
+  const videoId = getYoutubeVideoId(url)
+  if (!videoId) {
+    return ''
+  }
+  return `<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${videoId}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
 }
 
 const mdParser: any = new MarkdownIt({
@@ -29,7 +47,33 @@ const mdParser: any = new MarkdownIt({
 
     return '' // use external default escaping
   },
-}).use(insert)
+})
+  .use(insert)
+  .use(markdownItContainer, 'youtube', {
+    validate: function (params: string) {
+      return params.trim().match(/^youtube$/)
+    },
+    render: function (tokens: Token[], idx: number) {
+      if (tokens[idx].type === 'container_youtube_open') {
+        let urlsIndex
+        if (tokens[idx + 1].type === 'code_block') {
+          urlsIndex = idx + 1
+        } else {
+          urlsIndex = idx + 2
+        }
+        const urls = tokens[urlsIndex].content
+          .split('\n')
+          .map((url) => url.trim())
+        let markup = '<div>'
+        for (const url of urls) {
+          markup += getYoutubeIframeMarkup({ url })
+        }
+        return markup
+      } else if (tokens[idx].type === 'container_youtube_close') {
+        return '</div>'
+      }
+    },
+  })
 
 export default function MarkdownEditor({
   inEditMode,
@@ -44,7 +88,7 @@ export default function MarkdownEditor({
 }) {
   const handleEditorChange = ({ html, text }: any) => {
     setEditorState(text)
-    console.log('handleEditorChange', html, text)
+    // console.log('handleEditorChange', html, text)
   }
 
   if (!process.browser) {
