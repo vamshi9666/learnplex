5d013cb2fb4596b98e5665c2d337fc24a297c16d
Bhanu Teja P <pbteja1998@gmail.com>
2020-05-13 10:52:29 +0530

    Show 'Start Learning' button on resource index page

--- src/components/learn/ResourceIndex.tsx
+++ src/components/learn/ResourceIndex.tsx
@@ -1,7 +1,12 @@
 import SectionItems from './SectionItems'
 import React from 'react'
+import { Button, Skeleton } from 'antd'
+import { useMutation, useQuery } from 'urql'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
 
 import { Section } from '../../graphql/types'
+import { useSections } from '../../lib/hooks/useSections'
 
 export default function ViewResourceIndex({
   baseSectionId,
@@ -13,12 +18,74 @@ export default function ViewResourceIndex({
   username: string
 }) {
   const baseSection = sectionsMap.get(baseSectionId)!
+  const HAS_ENROLLED_QUERY = `
+    query($username: String!, $resourceSlug: String!) {
+      hasEnrolled(username: $username, resourceSlug: $resourceSlug)
+    }
+  `
+  const router = useRouter()
+  const resourceSlug = router.query.resource as string
+  const { resourceId } = useSections({ resourceSlug, username })
+
+  const START_PROGRESS_MUTATION = `
+    mutation($resourceId: String!) {
+      startProgress(resourceId: $resourceId) {
+        id
+        resource {
+          slug
+          user {
+            username
+          }
+          firstPageSlugsPath
+        }
+      }
+    }
+  `
+
+  const [, startProgressMutation] = useMutation(START_PROGRESS_MUTATION)
+
+  const [{ data, fetching }, reExecuteHasEnrolledQuery] = useQuery({
+    query: HAS_ENROLLED_QUERY,
+    variables: {
+      username,
+      resourceSlug,
+    },
+  })
+
+  if (fetching) {
+    return <Skeleton active={true} />
+  }
+
+  const startProgress = () => {
+    NProgress.start()
+    startProgressMutation({ resourceId }).then(async (result) => {
+      if (result.error) {
+        console.log({ startProgressError: result.error })
+      } else {
+        console.log({ result })
+        reExecuteHasEnrolledQuery()
+      }
+    })
+    NProgress.done()
+  }
+
   return (
-    <SectionItems
-      sections={baseSection.sections}
-      sectionsMap={sectionsMap}
-      topLevel={true}
-      username={username}
-    />
+    <>
+      <SectionItems
+        sections={baseSection.sections}
+        sectionsMap={sectionsMap}
+        topLevel={true}
+        username={username}
+      />
+      <div className={'p-5'}>
+        {!data?.hasEnrolled ? (
+          <Button type={'primary'} onClick={() => startProgress()}>
+            Start Learning
+          </Button>
+        ) : (
+          <Button type={'primary'}>Already Enrolled</Button>
+        )}
+      </div>
+    </>
   )
 }
